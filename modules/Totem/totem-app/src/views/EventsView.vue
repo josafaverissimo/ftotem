6da4e89faca11486487f12bb9e447244080d0e37
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-12 01:37:52 -0300

    change: event card show with big size

--- modules/Totem/totem-app/src/views/EventsView.vue
+++ modules/Totem/totem-app/src/views/EventsView.vue
@@ -1,20 +1,29 @@
 <script setup>
 import Header from '@/components/Header.vue'
 import EventCard from '@/components/EventCard.vue'
-import { useEventsStore } from '@/stores/events.js'
-import { computed } from "vue";
+import MySelect from '@/components/MySelect.vue'
 import router from "@/router/index.js";
+import { useEventsStore } from '@/stores/events.js'
+import { computed, reactive } from "vue";
 
 const eventsStore = useEventsStore()
-const eventsByCategoriesKeys = computed(() => Object.keys(eventsStore.eventsByCategories))
+const currentEvent = reactive({
+  data: null
+})
+const eventsOptions = computed(() => eventsStore.data.map((event, index) => ({
+  value: index,
+  textContent: event.name
+})))
+const currentEventImg = computed(() =>
+    `${import.meta.env.VITE_BASE_URL}/uploads/events/${currentEvent.data.background}`)
 
 function logout() {
   eventsStore.$reset()
   router.push({name: 'login'})
 }
 
-function getEventImgPath(event) {
-  return `${import.meta.env.VITE_BASE_URL}/uploads/events/${event.background}`
+function setCurrentEventByOptionValue(option) {
+  currentEvent.data = eventsStore.data[option.value]
 }
 
 eventsStore.loadEvents()
@@ -23,37 +32,28 @@ eventsStore.loadEvents()
 
 <template>
   <div class="wrapper">
-    <div>
-      <Header>
-        <h1 class="display-2 fw-bolder">Eventos</h1>
-      </Header>
-
-      <hr>
-    </div>
+    <Header>
+      <h1 class="display-2 fw-bolder">Eventos</h1>
+    </Header>
 
     <div class="events-wrapper">
       <div class="actions-buttons-wrapper">
-        <button class="btn btn-primary rounded-5" @click="eventsStore.loadEvents"
-          :class="!eventsStore.notHasPageToLoad ? '' : 'disabled pe-none'">
-          <i class="bi bi-plus-circle fs-4"></i>
-        </button>
-
         <button class="btn btn-dark rounded-5" @click="logout">
           <i class="bi bi-escape fs-5"></i>
         </button>
       </div>
 
       <div class="container">
-        <template v-for="(category, index) in eventsByCategoriesKeys" :key="index">
+        <div class="event-metadata-wrapper">
           <div class="event-category">
-            <span class="text-capitalize fs-4">{{category}}</span>
+            <span class="text-capitalize fs-4" v-if="currentEvent.data">{{currentEvent.data.category}}</span>
           </div>
 
-          <div class="row row-cols-1 row-cols-lg-3 g-3">
-            <template v-for="(event, index) in eventsStore.eventsByCategories[category]" :key="index">
-              <EventCard :imgSrc="getEventImgPath(event)" :title="event.name"/>
-            </template>
-          </div>
+          <MySelect :options="eventsOptions" @change="setCurrentEventByOptionValue"/>
+        </div>
+
+        <template v-if="currentEvent.data">
+          <EventCard :imgSrc="currentEventImg" :title="currentEvent.data.name"/>
         </template>
       </div>
     </div>
@@ -66,17 +66,16 @@ eventsStore.loadEvents()
   flex-direction: column;
   height: 100vh;
 
-  hr {
-    margin: 0;
-  }
-
   .events-wrapper {
     height: 100%;
     overflow-y: scroll;
 
-    .event-category {
-      border-bottom: 1px solid #e0e0e0;
-      margin: 1rem 0;
+    .event-metadata-wrapper {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+      padding: .5rem;
+      margin: .5rem 0;
     }
 
     .actions-buttons-wrapper {
