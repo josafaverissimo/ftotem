e4b1c770bc89261419bf718f4f14e2e979137b2a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-12 15:05:47 -0300

    refactor: event clients table separated from eventCard. It's now a component

--- modules/Totem/totem-app/src/components/EventCard.vue
+++ modules/Totem/totem-app/src/components/EventCard.vue
@@ -15,35 +15,11 @@ onMounted(() => {
 
 <template>
   <div class="event-card-wrapper">
-    <div class="body">
-      <div class="img-title-wrapper">
-        <span class="title text-capitalize">{{props.title}}</span>
+    <div class="img-title-wrapper">
+      <span class="title text-capitalize">{{props.title}}</span>
 
-        <div class="img-wrapper shadow">
-          <img :src="props.imgSrc" alt="event image" loading="lazy"/>
-        </div>
-      </div>
-
-      <div class="action-wrapper">
-        <div class="table-wrapper">
-          <span class="h2 text-center w-100">Lista de clientes</span>
-
-          <table class="table">
-            <thead>
-              <tr>
-                <th>Nome</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(client, index) in clients" :key="index">
-                <td class="text-capitalize truncate" data-bs-toggle="tooltip" data-bs-placement="bottom"
-                  :data-bs-title="client" ref="tooltips">
-                  {{client}}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div class="img-wrapper shadow">
+        <img :src="props.imgSrc" alt="event image" loading="lazy"/>
       </div>
     </div>
   </div>
@@ -51,62 +27,40 @@ onMounted(() => {
 
 <style scoped>
 .event-card-wrapper {
-  .body {
-    width: 100%;
+  .img-title-wrapper {
+    object-fit: contain;
+    cursor: pointer;
     display: flex;
+    flex-direction: column;
+    align-items: center;
 
-    .img-title-wrapper {
-      object-fit: contain;
-      cursor: pointer;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
-      .title {
-        background-color: #2f2f2f;
-        color: #fff;
-        border-top-right-radius: 1rem;
-        border-top-left-radius: 1rem;
-        padding: .5rem 1rem;
-        text-align: center;
-        width: 20rem;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        box-shadow: 1px 1px 16px #0002;
-        position: relative;
-        z-index: -1;
-      }
-
-      .img-wrapper {
-        border-radius: 1rem;
-        background-color: #fff;
-        width: 90%;
-
-        img {
-          width: 100%;
-          max-height: 40rem;
-          object-fit: contain;
-          border-radius: 1rem;
-          padding: .5rem;
-        }
-      }
+    .title {
+      background-color: #2f2f2f;
+      color: #fff;
+      border-top-right-radius: 1rem;
+      border-top-left-radius: 1rem;
+      padding: .5rem 1rem;
+      text-align: center;
+      width: 20rem;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      box-shadow: 1px 1px 16px #0002;
+      position: relative;
+      z-index: -1;
     }
 
-    .action-wrapper {
-      flex-grow: 1;
+    .img-wrapper {
+      border-radius: 1rem;
+      background-color: #fff;
+      width: 90%;
 
-      .table-wrapper {
-        margin: 0 1rem;
-        display: flex;
-        flex-direction: column;
-
-        .truncate {
-          max-width: 0;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          overflow: hidden;
-        }
+      img {
+        width: 100%;
+        max-height: 40rem;
+        object-fit: contain;
+        border-radius: 1rem;
+        padding: .5rem;
       }
     }
   }
---
+++ modules/Totem/totem-app/src/components/EventClientsTable.vue
@@ -0,0 +1,47 @@
+<script setup>
+const props = defineProps(['clients'])
+</script>
+
+<template>
+  <div class="table-wrapper">
+    <span class="h2 text-center w-100">Lista de clientes</span>
+
+    <div class="table-container">
+      <table class="table table-striped w-100">
+        <thead>
+        <tr>
+          <th>Nome</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(client, index) in props.clients" :key="index">
+          <td class="text-capitalize truncate" data-bs-toggle="tooltip" data-bs-placement="bottom"
+              :data-bs-title="client" ref="tooltips">
+            {{client}}
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.table-wrapper {
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+
+  .table-container {
+    height: 40rem;
+    overflow-y: auto;
+  }
+
+  .truncate {
+    max-width: 0;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+}
+</style>
