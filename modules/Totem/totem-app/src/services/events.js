4ed97dc06f6b9cac977bc17296a0cc9797f02191
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-12 01:15:28 -0300

    change: events services requests to getAll and event store just save all events

--- modules/Totem/totem-app/src/services/events.js
+++ modules/Totem/totem-app/src/services/events.js
@@ -1,6 +1,6 @@
 import {http} from '@/utils/http.js'
 
 export function getEvents(page) {
-    return http.get(`totem-api/events/get?page=${page}`)
+    return http.get(`totem-api/events/getAll`)
         .then(response => response.data)
 }
