f9ee5904c6bb8d82b9213b9e1aa8af9421cb1e46
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-13 23:50:52 -0300

    build: totem-app

--- totem/assets/Spinner-EhloaD3e.js
+++
@@ -1,1 +0,0 @@
-import{o as r,a,n}from"./index-XAfTw5kq.js";const l={__name:"Spinner",props:["class"],setup(s){const e=s;return(o,c)=>(r(),a("div",{class:n(["spinner-border",e.class]),role:"status"},null,2))}};export{l as _};
