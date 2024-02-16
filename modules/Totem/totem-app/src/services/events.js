dc2fc7f2503a7b58a8f198e4509c8d9e61199210
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-16 00:42:35 -0300

    add: get events by hash method added

--- modules/Totem/totem-app/src/services/events.js
+++ modules/Totem/totem-app/src/services/events.js
@@ -4,3 +4,7 @@ export function getEvents(page) {
     return http.get(`totem-api/events/getAll`)
         .then(response => response.data)
 }
+
+export function getByHash(hash) {
+    return http.get(`totem-api/events/getByHash/${hash}`).then(response => response.data)
+}
