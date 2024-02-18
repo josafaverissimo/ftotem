f2a6a1cde6bcb1c1bb1ab48acec69b23fe85047a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-17 22:15:54 -0300

    style: header remove padding and img size is bigger

--- modules/Manager/Views/Partials/header.php
+++ modules/Manager/Views/Partials/header.php
@@ -2,7 +2,7 @@
     /** @var string $pageHeader */
 ?>
 
-<header class="w-100 px-4 pt-4 pb-2 border-bottom">
+<header class="w-100 px-4 py-1 border-bottom">
     <h1 class="h2 fw-bold animate__animated animate__fadeInLeft page-title"><?= $pageHeader ?></h1>
     <img src="<?= base_url('assets/imgs/logo.png') ?>" alt="relive logo"
         class="animate__animated animate__fadeInRightBig">
