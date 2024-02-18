d59b4f877b87f75dfa1cc05f841d9f824062d701
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-02-18 01:29:10 -0300

    feat: table columns translation

---
+++ app/Language/pt-br/Words.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'id' => 'id',
+    'cpf' => 'cpf',
+    'cep' => 'cep',
+    'name' => 'nome',
+    'username' => 'usuário',
+    'category' => 'categoria',
+    'background' => 'plano de fundo',
+    'clients' => 'clientes',
+    'active' => 'ativo',
+    'created_at' => 'criado em',
+    'cellphone' => 'telefone',
+    'state' => 'estado',
+    'city' => 'cidade',
+    'address' => 'logradouro',
+    'neighborhood' => 'bairro',
+    'house_number' => 'número da casa'
+];
