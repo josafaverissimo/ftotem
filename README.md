afa53e6c7a459ad53f9a7797460fc725f5bd5820
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-04-13 09:28:40 -0300

    upgrade: Codeigniter upgrade to 4.5.0 and also all dependecies

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# FTOTEM Project
+# 
--- app/Helpers/views_helper.php
+++ app/Helpers/views_helper.php
@@ -4,7 +4,7 @@ use Config\View;
 
 function newView(string $viewPath, array $data = []): string {
     $config = config(View::class);
-    $view = new \CodeIgniter\View\View($config);
+    $view = new \CodeIgniter\View\View($config, $viewPath);
 
     return $view->setData($data)->render($viewPath);
 }
